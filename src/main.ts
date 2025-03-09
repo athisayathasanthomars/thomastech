31287a877a0b62f971a6652160ef9215136e4466
athisayathasanthomars <thomarsthuvaa@gmail.com>
2025-03-09 21:49:43 +0530

    nav routes.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { Routes,provideRouter } from '@angular/router';
-import { HomePageComponent } from './app/home-page/home-page.component';
-import { AboutPageComponent } from './app/about-page/about-page.component';
-import { CoursesPageComponent } from './app/courses-page/courses-page.component';
-import { ContactusPageComponent } from './app/contactus-page/contactus-page.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-const routes:Routes=[
-  {path:'',component:HomePageComponent},
-  {path:'home',component:HomePageComponent},
-  {path:'aboutus',component:AboutPageComponent},
-  {path:'courses',component:CoursesPageComponent},
-  {path:'contactus',component:ContactusPageComponent},
-  {path:'**',component:HomePageComponent},
-];
 
 bootstrapApplication(AppComponent,appConfig)
   .catch((err) => console.error(err));
