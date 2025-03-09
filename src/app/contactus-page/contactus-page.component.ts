0edc01a641738c758ea7c7e9594f3a58e489e89e
athisayathasanthomars <thomarsthuvaa@gmail.com>
2025-03-09 20:26:27 +0530

    before contact spring

--- src/app/contactus-page/contactus-page.component.ts
+++ src/app/contactus-page/contactus-page.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl,FormGroup,ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contactus-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contactus-page.component.html',
   styleUrl: './contactus-page.component.scss'
 })
 export class ContactusPageComponent {
+  customer=new FormGroup({
+    firstname:new FormControl(""),
+    lastname:new FormControl(""),
+    email:new FormControl(""),
+    phoneno:new FormControl(""),
+    message:new FormControl("")
 
+  });
+  
+  submittedfname = '';
+  submittedlname = '';
+  submittedemail = '';
+  submittedphoneno = '';
+  submittedmessage = '';
+  GetDetails(){
+    this.submittedfname=this.customer.value.firstname ?? "";
+    this.submittedlname=this.customer.value.lastname ?? "";
+    this.submittedemail=this.customer.value.email ?? "";
+    this.submittedphoneno=this.customer.value.phoneno ?? "";
+    this.submittedmessage=this.customer.value.message ?? "";
+    console.log(this.submittedfname);
+    console.log(this.submittedlname);
+    this.customer.reset();
+  }
 }
